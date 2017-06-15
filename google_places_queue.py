6364827b6ee965ae28687909e91ec5d727c7d541
Cody Taylor <ctaylor4874@gmail.com>
2017-06-15 08:33:56 -0500

    Increased sleep timer, refactored to work with new credentials class.

--- google_places_queue.py
+++ google_places_queue.py
@@ -21,11 +21,11 @@ engine = sqlalchemy.create_engine(os.getenv('HAPPYFINDER_ENGINE'), encoding='utf
 Session = sessionmaker(engine)
 
 INSERT_QUERY = """
-       INSERT INTO happyfinder_schema.happyfinder(happyfinder.name, lat, lng, hours,
-       rating, phone_number, address, url, google_id, price, fs_venue_id)
-       VALUES(:v_name, :lat, :lng, :hours, :rating, :phone_number, :address, 
-       :url, :google_id, :price, :fs_venue_id);
-       """
+    INSERT INTO happyfinder_schema.happyfinder(happyfinder.name, lat, lng, hours,
+    rating, phone_number, address, url, google_id, price)
+    VALUES(:v_name, :lat, :lng, :hours, :rating, :phone_number, :address, 
+    :url, :google_id, :price);
+    """
 
 
 def get_message(queue):
@@ -41,18 +41,13 @@ def check_errors(response):
     return response
 
 
-def send_message(queue, parsed_data):
-    data = {
-        'lat': parsed_data.lat,
-        'lng': parsed_data.lng,
-        'name': parsed_data.name
-    }
+def send_message(queue, data):
+
     response = queue.send_message(MessageBody=json.dumps(data))
     check_errors(response)
 
 
-def insert_data(data, fs_venue_id):
-    if fs_venue_id:
+def insert_data(data):
         with closing(Session()) as s:
             try:
                 s.execute(INSERT_QUERY, params={
@@ -69,7 +64,6 @@ def insert_data(data, fs_venue_id):
                     'url': data.url.encode('utf-8') if data.url else None,
                     'google_id': data.google_id.encode('utf-8') or None,
                     'price': data.price if data.price else None,
-                    'fs_venue_id': fs_venue_id.encode('utf-8') if fs_venue_id else None
                 })
             except IntegrityError or Exception as err:
                 s.rollback()
@@ -81,36 +75,31 @@ def insert_data(data, fs_venue_id):
                 s.commit()
 
 
-def get_fs_venue_id(parsed_data):
-    response = s.get('https://api.foursquare.com/v2/venues/search?intent=match&ll={},{}&query={}'.format(
-        parsed_data.lat, parsed_data.lng, parsed_data.name
-    ))
-    str_response = response.content.decode('utf-8')
-    fs_data = FoursquareDetails(json.loads(str_response))
-    return fs_data.fs_venue_id
-
 
 def make_request(queue, message):
     api = APIHandler(message)
     api_data = api.get_load()
     parsed_data = GoogleDetails(api_data)
-    fs_venue_id = get_fs_venue_id(parsed_data)
-    insert_data(parsed_data, fs_venue_id)
-    send_message(queue, parsed_data)
+    insert_data(parsed_data)
+    data = {
+        'lat': parsed_data.lat,
+        'lng': parsed_data.lng,
+        'name': parsed_data.name,
+        'google_id': parsed_data.google_id
+    }
+    send_message(queue, data)
 
 
 def run():
-    credentials_alternator = Alternator()
     google_places_queue = sqs.get_queue(BOTO_QUEUE_NAME_PLACES)
     foursquare_details_queue = sqs.get_queue(BOTO_QUEUE_NAME_FS_DETAILS)
     while True:
         message = get_message(google_places_queue)
         if not message:
             logging.info(os.path.basename(__file__))
-            time.sleep(5)
+            time.sleep(30)
             continue
-        credentials = credentials_alternator.toggle_foursquare_values()
-        make_request(foursquare_details_queue, message.body, credentials)
+        make_request(foursquare_details_queue, message.body)
         delete_message(google_places_queue, message)
 
 
