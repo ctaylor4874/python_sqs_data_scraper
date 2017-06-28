b97233e2782df5272f70a9b05655502a5838c1da
Cody Taylor <ctaylor4874@gmail.com>
2017-06-28 16:16:14 -0500

    Refactored.

--- fs_details_queue.py
+++ fs_details_queue.py
@@ -24,24 +24,6 @@ engine = sqlalchemy.create_engine(os.getenv('HAPPYFINDER_ENGINE'), encoding='utf
 Session = sessionmaker(engine)
 
 
-def get_message(queue):
-    message = queue.receive_messages(MaxNumberOfMessages=1)
-    if not message:
-        return None
-    return message[0]
-
-
-def check_errors(response):
-    if response.get('ResponseMetadata', '').get('HTTPStatusCode', '') is not 200:
-        logging.info('ERROR! {}'.format(response))
-    return response
-
-
-def send_message(queue, url):
-    response = queue.send_message(MessageBody=url)
-    check_errors(response)
-
-
 def make_url(data, credentials):
     url = "https://api.foursquare.com/v2/venues/{}/menu?client_id={}&client_secret={}&v=20170109".format(
         data.fs_venue_id, credentials.foursquare_client_id, credentials.foursquare_client_secret)
@@ -69,7 +51,7 @@ def make_request(queue, message, credentials):
             'category': parsed_data.category,
             'fs_venue_id': parsed_data.fs_venue_id
         }
-        send_message(queue, json.dumps(data))
+        sqs.send_message(queue, json.dumps(data))
     else:
         delete(parsed_data.fs_venue_id)
 
@@ -79,7 +61,7 @@ def run():
     fs_details_queue = sqs.get_queue(BOTO_QUEUE_NAME_FS_DETAILS)
     menu_queue = sqs.get_queue(BOTO_QUEUE_NAME_FS_MENU)
     while True:
-        message = get_message(fs_details_queue)
+        message = sqs.get_message(fs_details_queue)
         if not message:
             logging.info(os.path.basename(__file__))
             time.sleep(5)
@@ -87,7 +69,7 @@ def run():
         logging.info('menu queue: {}\nmessage.body:{}'.format(menu_queue, message.body))
         credentials = credentials_alternator.toggle_foursquare_values()
         make_request(menu_queue, message.body, credentials)
-        delete_message(fs_details_queue, message)
+        sqs.delete_message(fs_details_queue, message)
 
 
 if __name__ == '__main__':
